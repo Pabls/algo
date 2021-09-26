b2e35b2a08e31413b1c59713e72f388c8f532e38
p.semin <p.semin@city-mobil.ru>
2021-09-26 10:52:01 +0300

    add task number 4

--- openedu/week_1_task_4/Main.java
+++ openedu/week_1_task_4/Main.java
@@ -1,5 +1,41 @@
+import java.io.*;
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        
+        try {
+            File inputFile = new File("input.txt");
+            Scanner inputScanner = new Scanner(inputFile);
+
+            int arraySize = Integer.valueOf(inputScanner.nextLine()).intValue();
+            String[] strings = inputScanner.nextLine().split(" ");
+
+            double[][] digits = new double[arraySize][2];
+
+            for(int i = 0; i < strings.length; i++) {
+                digits[i][0] = Double.valueOf(strings[i]).doubleValue();
+                digits[i][1] = i + 1;
+            }
+
+            for(int j = 0; j < digits.length; j++) {
+                int tmpIndex = j - 1;
+                while (tmpIndex >= 0 && digits[tmpIndex][0] > digits[tmpIndex + 1][0]) {
+                    double[] tmp = digits[tmpIndex];
+                    digits[tmpIndex] = digits[tmpIndex + 1];
+                    digits[tmpIndex + 1] = tmp;
+                    tmpIndex -= 1;
+                }
+            }
+            
+            int min = (int) digits[0][1];
+            int avg = (int) digits[arraySize / 2][1];
+            int max = (int) digits[arraySize - 1][1];
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            writer.write(min + " " + avg + " " + max);
+            writer.close();
+        } catch(Exception ex) {
+            System.out.println(ex);
+        }
     }
 }
