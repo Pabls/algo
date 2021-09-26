9cb83d061e4108b3760c563aff6f4cb92ddf1654
p.semin <p.semin@city-mobil.ru>
2021-09-26 11:43:14 +0300

    add task 5

---
+++ openedu/week_1_task_5/Main.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        try {
+            File inputFile = new File("input.txt");
+            Scanner inputScanner = new Scanner(inputFile);
+
+            int arraySize = Integer.valueOf(inputScanner.nextLine()).intValue();
+            String[] strings = inputScanner.nextLine().split(" ");
+
+            int[] digits = new int[arraySize];
+
+            for(int i = 0; i < strings.length; i++) {
+                digits[i] = Integer.valueOf(strings[i]).intValue();
+            }
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            for(int j = 0; j < digits.length; j++) {
+                int tmpIndex = j - 1;
+                while (tmpIndex >= 0 && digits[tmpIndex] > digits[tmpIndex + 1]) {
+                    int tmp = digits[tmpIndex];
+                    digits[tmpIndex] = digits[tmpIndex + 1];
+                    digits[tmpIndex + 1] = tmp;
+                    tmpIndex -= 1;
+                }
+                if((tmpIndex + 2) != (j + 1)) {
+                    writer.write("Swap elements at indices " + (tmpIndex + 2) + " and " + (j + 1) + ".");
+                    writer.newLine();
+                }
+            }
+            writer.write("No more swaps needed.");
+            writer.close();
+        } catch(Exception ex) {
+            System.out.println(ex);
+        }
+    }
+}
